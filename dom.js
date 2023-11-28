ccd60d13cec874aeaab8174f2d9b6a1d8ac8bb54
Giovanna Franco <rm553701@fiap.com.br>
2023-11-28 16:43:47 -0300

    GS

---
+++ dom.js
@@ -0,0 +1,58 @@
+const pMensagem = document.createElement('p');
+const campoNome = document.querySelector('#nome');
+const campoEmail= document.querySelector('#email');
+
+const buttonEnviar = document.querySelector("button");
+const popup = document.querySelector('.popup-wraper');
+
+
+function clearForm(){
+    campoNome.value = '';
+    campoEmail.value = '';
+}
+
+const openClick = function(){
+    popup.classList.add('d-block');
+}
+
+const eventModal = function(event){
+    const clickOfElement = event.target.classList[0];
+    const classListNameArray = ['popup-close', 'popup-wraper'];
+    const isClassList = classListNameArray.includes(clickOfElement);
+    if (isClassList){
+        popup.classList.remove("d-block");
+
+    }
+}
+
+buttonEnviar.addEventListener('click',function(event){
+    event.preventDefault();
+    if (campoNome.value ===''){
+        pMensagem.textContent = 'Campo Obrigatório';
+        campoNome.insertAdjacentElement('afterend', pMensagem);
+        pMensagem.setAttribute('class', 'erro'); 
+        return False
+    }
+
+    if (campoEmail.value ===''){
+        pMensagem.textContent = 'Campo Obrigatório';
+        campoEmail.insertAdjacentElement('afterend', pMensagem);
+        pMensagem.setAttribute("class", 'erro');
+        return False
+    }
+    
+    else{
+        buttonEnviar.addEventListener("click", openClick);
+
+        popup.addEventListener('click', eventModal); 
+    }
+
+    pMensagem.classList.add('d-none');
+    clearForm();
+})
+
+
+
+
+
+
